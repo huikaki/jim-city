7e768380ad88fb48fc23687eb50e3cbb533e7ad6
kaki <>
2025-09-08 16:37:12 +0800

    Enhanced debug endpoint with password testing

--- api/debug/users.js
+++ api/debug/users.js
@@ -4,7 +4,7 @@ const { User } = require('../_lib/models');
 module.exports = async (req, res) => {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
@@ -13,6 +13,21 @@ module.exports = async (req, res) => {
 
   try {
     await connectDB();
+    
+    if (req.method === 'POST') {
+      // Test login credentials
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      
+      return res.status(200).json({
+        message: 'Login test',
+        input: { username, password },
+        userFound: !!user,
+        storedPassword: user ? user.password : 'N/A',
+        passwordMatch: user ? user.password === password : false,
+        userDetails: user ? { username: user.username, role: user.role } : null
+      });
+    }
     
     // Get all users
     const users = await User.find({});
@@ -28,14 +43,14 @@ module.exports = async (req, res) => {
       
       return res.status(200).json({
         message: 'Created default admin user',
-        users: [defaultUser],
+        users: [{ username: defaultUser.username, role: defaultUser.role, password: defaultUser.password }],
         totalUsers: 1
       });
     }
     
     return res.status(200).json({
       message: 'Users found',
-      users: users.map(u => ({ username: u.username, role: u.role })),
+      users: users.map(u => ({ username: u.username, role: u.role, password: u.password })),
       totalUsers: users.length
     });
     
